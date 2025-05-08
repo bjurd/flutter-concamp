661b35986c1ea799e1f517275ef35634dd458a69
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-08 13:32:37 -0400

    Base post fetcher from home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget
+import "package:cloud_firestore/cloud_firestore.dart";
+
+class Home extends StatefulWidget
+{
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>
 {
   Widget build(BuildContext context)
   {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+
+      body: SafeArea(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("posts")
+              .snapshots(),
+
+          builder: (context, snapshot)
+          {
+            if (!snapshot.hasData)
+              return Text("Thinking");
+
+            return Text("Yay!");
+          }
+        )
+      )
+    );
   }
 }
